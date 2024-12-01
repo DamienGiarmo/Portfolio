de539158a4fb739e5ca200083548ced5e69bf98c
dams dams <damien.giarmo@ynov.com>
2024-12-01 19:08:11 +0100

    correction code la c'est bon

---
+++ src/components/Projet.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Cardprojet from './Cardprojet';
+
+const Projet = () => {
+    return (
+        <div id="projet">
+            <h1 className="text-3xl font-bold text-center mb-8 text-[#C2E6FF]">Mes Projets</h1>
+            
+            <Cardprojet />
+        </div>
+    );
+};
+
+export default Projet;
